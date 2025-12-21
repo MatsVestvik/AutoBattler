b850455731d28497ccfa913121d17feb0a573ff3
MatsVestvik <mats.o.vestvik@gmail.com>
2025-12-21 14:18:26 +0100

    attempted to implemenent threads for updating scene when combart is complete

--- src/main/java/com/autobattler/scenes/FightScene.java
+++ src/main/java/com/autobattler/scenes/FightScene.java
@@ -8,6 +8,7 @@ import com.autobattler.character.Character;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.util.Duration;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -17,23 +18,30 @@ import javafx.stage.Stage;
 public class FightScene {
 
     public static void runFightScene(Stage primaryStage, Team playerTeam, Team enemyTeam) {
-        AutoBattle autoBattle = new AutoBattle();
-        autoBattle.battle(playerTeam, enemyTeam);
-
         YouLoseScene youLoseScene = new YouLoseScene();
         Scene loseScene = youLoseScene.createScene(primaryStage);
-
-        if(!playerTeam.hasAliveMembers()) {
-            primaryStage.setScene(loseScene);
-        } else if (!enemyTeam.hasAliveMembers()) {
-            primaryStage.setScene(loseScene);
-        }
 
         HBox root = new HBox(50);
         root.getChildren().add(playerTeam.getTeamView());
         root.getChildren().add(enemyTeam.getTeamView());
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
+        Scene battleScene = new Scene(root);
+        
+        Platform.runLater(() -> {
+            primaryStage.setScene(battleScene);
+        });
+        
+        // Run battle in background
+        new Thread(() -> {
+            AutoBattle autoBattle = new AutoBattle();
+            autoBattle.battle(playerTeam, enemyTeam);
+            
+            // Check result and switch to lose scene if needed
+            Platform.runLater(() -> {
+                if(!playerTeam.hasAliveMembers() || !enemyTeam.hasAliveMembers()) {
+                    primaryStage.setScene(loseScene);
+                }
+            });
+        }).start();
     }
     
 }
